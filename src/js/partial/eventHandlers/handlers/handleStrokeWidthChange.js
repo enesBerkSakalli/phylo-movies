649336d801b34006fa5e8879e57f972a280fb2e1
EnesSakalliUniWien <enes.sakalli@univie.ac.at>
2025-09-15 15:43:23 +0200

    Deploy site

--- src/js/partial/eventHandlers/handlers/handleStrokeWidthChange.js
+++
@@ -1,8 +0,0 @@
-import { useAppStore } from '../../../core/store.js';
-
-export async function handleStrokeWidthChange(event) {
-  const { setStrokeWidth, treeController } = useAppStore.getState();
-  setStrokeWidth(event.target.value);
-  document.getElementById('stroke-width-value').textContent = event.target.value;
-  await treeController.renderAllElements();
-}
